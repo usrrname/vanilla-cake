e6034fbcfc65c9b0e601b9d6e69926ab1099f8d3
Jen Chan <6406037+usrrname@users.noreply.github.com>
2019-05-10 12:39:11 -0400

    added show/hide navbar function

--- js/script.js
+++ js/script.js
@@ -1,22 +1,38 @@
-$(document).ready(function()
-{
-  $('.content').fadeIn();
+$(document).ready(function () {
+        $('.content').fadeIn();
 });
 
-//toggle between responsive menu in smaller screens
-const listItems= document.getElementsByTagName('ul').children;
-const noBootstrap = () => {
-        const nav = listItems;
-      console.log(listItems);
-                for (var i = 0; i < nav.length; i++) {
-                if (nav[i].firstChild.className !== 'responsive') {           
-                        nav[i].firstChild.className = "responsive";
-                      nav[i].firstChild.style.display = "inline-block";
-                }
-                else if (nav[i].firstChild.className === 'responsive'){
-                        nav[i].firstChild.className = "";
-                nav[i].firstChild.style.display = "none";
+function toggleNav() {
+        const el = document.getElementsByClassName('menu__navbar')[0];
+        this.show = () => {
+                el.style.display = 'block'
         }
-        }       
+        this.hide = () => {
+                el.style.display = 'none'
+        }
+        const value = el.style.display;
+        if (value == 'none') {
+                this.show();
+        }
+        if (value != 'none') {
+                this.hide();
+        }
+        return value;
+}
+
+//toggle between responsive menu in smaller screens
+const noBootstrap = () => {
+        const listItems = document.getElementsByTagName('ul').children;
+        const nav = listItems;
+        console.log(listItems);
+        for (var i = 0; i < nav.length; i++) {
+                if (nav[i].firstChild.className !== 'responsive') {
+                        nav[i].firstChild.className = "responsive";
+                        nav[i].firstChild.style.display = "inline-block";
+                } else if (nav[i].firstChild.className === 'responsive') {
+                        nav[i].firstChild.className = "";
+                        nav[i].firstChild.style.display = "none";
+                }
+        }
 }
 // function end
